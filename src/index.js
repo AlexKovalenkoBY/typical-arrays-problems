b17a3ea82dc3d68fba6004f358d31cba69286d63
AlexKovalenkoBY <kovalenkoam@gmail.com>
2020-10-03 20:58:37 +0300

    init: solved task with typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
+exports.min = function min(array) {
+    if ( (array === undefined) ||(array.length === 0)) {
+        return 0;
+    }
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        }
+    }
+    return min;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    
+    if ( (array === undefined) ||(array.length === 0)) {
+        return 0;
+    }
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    return max;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    
+    if ( (array === undefined) ||(array.length === 0)) {
+        return 0;
+    }
+    let avg = 0;
+    for (let i = 0; i < array.length; i++) {
+        avg = avg + array[i];
+    }
+    avg = avg / array.length;
+    return avg;
+};
